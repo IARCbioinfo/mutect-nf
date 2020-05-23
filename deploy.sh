e6405dab20ca0e75356d6782cef55024547950f4
nalcala <alcalan@fellows.iarc.fr>
2020-05-23 15:49:27 +0200

    configure deploy

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git add .
 git commit -m "Generated DAG [skip ci]"
 git push origin $CIRCLE_BRANCH
 
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/mutect-nf/trigger/848d00e1-4e32-498a-aaaa-4e17029cdf3c/
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://hub.docker.com/api/build/v1/source/33e58fde-38d2-40c6-b7d2-911a0fb2ab5b/trigger/848d00e1-4e32-498a-aaaa-4e17029cdf3c/call/
